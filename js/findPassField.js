9e94ed54befa39dbc23425288679087430b228b6
saanvi bhargava <saanvibhargava@saanvis-air.lan>
2020-03-11 20:02:12 -0700

    final before science fairr

--- js/findPassField.js
+++ js/findPassField.js
@@ -1,32 +1,53 @@
 var input_fields = document.getElementsByTagName("input")
 
-function insert_pass(e) {
-  alert("insert_pass")
-  // var current_url = window.location.href;
-  // var tab_arr = current_url.split("/");
-  // var passwd = "";
-  //
-  // current_url = tab_arr[0] +  "//"  + tab_arr[2]
-  // alert(current_url)
-  // chrome.storage.local.get('passwordlist', function(data) { 
-  //   alert("passlist fetched")
-  //
-  //   var g_passwordlist = data.passwordlist;
-  //   alert(JSON.stringify(g_passwordlist));
-  //   for (var j = 0; j < g_passwordlist.length; j++) {
-  //     if (current_url === g_passwordlist[j].url) {
-  //       alert("pass found")
-  //       f.value = g_passwordlist[j].passwd;
-  //     }
-  //   }
-  //   });
-  //   break;
+function insert_login(e) {
+  var f = e.target;
+  var current_url = window.location.href;
+  var tab_arr = current_url.split("/");
 
+  current_url = tab_arr[0] +  "//"  + tab_arr[2]
+  //alert(current_url)
+  chrome.storage.local.get('passwordlist', function(data) { 
+
+    var g_passwordlist = data.passwordlist;
+    //alert(g_passwordlist)
+    for (var j = 0; j < g_passwordlist.length; j++) {
+      if ((current_url === g_passwordlist[j].url) || (current_url+"/" === g_passwordlist[j].url)) {
+        f.value = g_passwordlist[j].login;
+      }
+    }
+    });
+    return;
+}
+
+function insert_pass(e) {
+  var f = e.target;
+  var current_url = window.location.href;
+  var tab_arr = current_url.split("/");
+
+  var passwd = "";
+
+  current_url = tab_arr[0] +  "//"  + tab_arr[2]
+  //alert(current_url)
+  chrome.storage.local.get('passwordlist', function(data) { 
+
+    var g_passwordlist = data.passwordlist;
+    //alert(g_passwordlist)
+    for (var j = 0; j < g_passwordlist.length; j++) {
+      if ((current_url === g_passwordlist[j].url) || (current_url+"/" === g_passwordlist[j].url)) {
+        f.value = g_passwordlist[j].passwd;
+      }
+    }
+    });
+    return;
 }
 
 for (var i = 0;i < input_fields.length; i++) {
   var f = input_fields[i];
   if (f.type == "password") {
-    f.addEventListener("click", insert_pass())
+    f.addEventListener("click", insert_pass)
+  }
+  if (f.type == "text") {
+    f.addEventListener("click", insert_login)
   }
 }
